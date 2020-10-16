2202db63dbe9cc4da61a7fd1dc3755bc31b53c7d
Nhi Pham <npham@amperecomputing.com>
2020-10-16 23:04:25 +0700

    Silicon/Ampere: Fixup runtime memory attribute
    This adds new FixupMemoryMap module to scan the runtime memory and apply
    the XP memory attribute as OS does not complain about missing RO and XP
    attributes.
    
    Signed-off-by: Nhi Pham <npham@amperecomputing.com>
    Signed-off-by: Phong Vo <pvo@amperecomputing.com>

---
+++ Silicon/Ampere/Drivers/FixupMemoryMap/FixupMemoryMap.c
@@ -0,0 +1,93 @@
+/** @file
+
+  Copyright (c) 2020, Ampere Computing LLC. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Base.h>
+#include <Uefi/UefiSpec.h>
+#include <Guid/EventGroup.h>
+#include <Guid/MemoryAttributesTable.h>
+#include <Protocol/Cpu.h>
+#include <Library/DebugLib.h>
+#include <Library/UefiLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+
+EFI_CPU_ARCH_PROTOCOL             *mCpu;
+
+/**
+  Notify function for event group EVT_SIGNAL_EXIT_BOOT_SERVICES.
+
+  @param[in]  Event   The Event that is being processed.
+  @param[in]  Context The Event Context.
+
+**/
+VOID
+EFIAPI
+OnExitBootServices(
+  IN EFI_EVENT        Event,
+  IN VOID             *Context
+  )
+{
+  EFI_STATUS                    Status;
+  EFI_MEMORY_ATTRIBUTES_TABLE   *MemoryAttributesTable;
+  EFI_MEMORY_DESCRIPTOR         *Desc;
+  UINTN                         Index;
+
+  DEBUG ((EFI_D_INFO, "%a:%d +\n", __FUNCTION__, __LINE__));
+
+  Status = gBS->LocateProtocol (&gEfiCpuArchProtocolGuid, NULL, (VOID **) &mCpu);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((EFI_D_INFO, "%a:%d -\n", __FUNCTION__, __LINE__));
+    return;
+  }
+
+  Status = EfiGetSystemConfigurationTable (&gEfiMemoryAttributesTableGuid, (VOID **) &MemoryAttributesTable);
+  if (EFI_ERROR (Status) || MemoryAttributesTable == NULL) {
+    DEBUG ((EFI_D_INFO, "%a:%d -\n", __FUNCTION__, __LINE__));
+    return;
+  }
+
+  Desc = (EFI_MEMORY_DESCRIPTOR *) ((UINT64) MemoryAttributesTable + sizeof (EFI_MEMORY_ATTRIBUTES_TABLE));
+  for (Index = 0; Index < MemoryAttributesTable->NumberOfEntries; Index++) {
+    if (Desc->Type != EfiRuntimeServicesCode && Desc->Type != EfiRuntimeServicesData) {
+      Desc = (EFI_MEMORY_DESCRIPTOR *)((UINT64) Desc + MemoryAttributesTable->DescriptorSize);
+      continue;
+    }
+
+    if (!(Desc->Attribute & (EFI_MEMORY_RO | EFI_MEMORY_XP))) {
+      Desc->Attribute |= EFI_MEMORY_XP;
+      mCpu->SetMemoryAttributes (mCpu, Desc->PhysicalStart, EFI_PAGES_TO_SIZE (Desc->NumberOfPages), Desc->Attribute);
+      DEBUG ((EFI_D_INFO, "%a: Set memory attribute, Desc->PhysicalStart=0x%X, size=%d, Attributes=0x%X\n",
+                          __FUNCTION__, Desc->PhysicalStart, EFI_PAGES_TO_SIZE (Desc->NumberOfPages), Desc->Attribute));
+    }
+    Desc = (EFI_MEMORY_DESCRIPTOR *)((UINT64) Desc + MemoryAttributesTable->DescriptorSize);
+  }
+
+  DEBUG ((EFI_D_INFO, "%a:%d -\n", __FUNCTION__, __LINE__));
+}
+
+EFI_STATUS
+EFIAPI
+FixupMemoryMapInitialize (
+  IN EFI_HANDLE         ImageHandle,
+  IN EFI_SYSTEM_TABLE   *SystemTable
+  )
+{
+  EFI_EVENT             ExitBootServicesEvent;
+  EFI_STATUS            Status;
+
+  Status = gBS->CreateEvent (
+                  EVT_SIGNAL_EXIT_BOOT_SERVICES,
+                  TPL_NOTIFY,
+                  OnExitBootServices,
+                  NULL,
+                  &ExitBootServicesEvent);
+  ASSERT_EFI_ERROR (Status);
+
+  return EFI_SUCCESS;
+}
