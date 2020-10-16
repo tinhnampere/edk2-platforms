4259e3adf0b2b2165aebd47702bab22183922c77
Nhi Pham <npham@amperecomputing.com>
2020-10-16 14:58:51 +0700

    AmpereAltraPkg: Implement PlatformFlashAccessLib instance
    This library provides API functions to perform flash write operations
    via Trusted Firmware SMC interface.
    
    The platform flash device is controlled by Arm Trusted Firmware (ATF)
    in the secure world. Hence, UEFI can not write directly an update image
    to the flash device. So, this instance of PlatformFlashAccessLib is
    implemented to transfer the update image to ATF secure world via the
    Firmware Update SMC service. Then that service will proceed the update.
    
    Signed-off-by: Nhi Pham <npham@amperecomputing.com>
    Signed-off-by: Phong Vo <pvo@amperecomputing.com>

---
+++ Silicon/Ampere/AmpereAltraPkg/Library/PlatformFlashAccessLibSmc/PlatformFlashAccessLibSmc.c
@@ -0,0 +1,247 @@
+/** @file
+  Platform flash device access library.
+
+  Copyright (c) 2016 - 2018, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2020, Ampere Computing LLC. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <PiDxe.h>
+
+#include <Library/BaseMemoryLib.h>
+#include <Library/PlatformFlashAccessLib.h>
+#include <Library/DebugLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/ArmSmcLib.h>
+
+/*
+ * SMC Function ID for Firmware Update service
+ */
+#define SMC_FWU_SERVICE               0xC300FF09
+
+/*
+ * SMC return codes
+ */
+#define SMC_OK                        (0)
+#define SMC_ACCESS_DENIED             (-1)
+#define SMC_NOT_SET                   (-2)
+#define SMC_IO_ERROR                  (-5)
+#define SMC_INSUFFICIENT_RES          (-28)
+#define SMC_AUTH_ERROR                (-80)
+#define SMC_FWU_IN_PROGRESS           (-36)
+
+/*
+ * The ARM Trusted Firmware (ATF) defined image types to support updating
+ * various firmware via SMC Firmware Update service.
+ * The value of definition here must be the same in ATF code.
+ */
+#define FWU_IMG_TYPE_INVALID   0
+#define FWU_IMG_TYPE_ATFBIOS   2 // ARM Trusted Firmware and UEFI Image
+#define FWU_IMG_TYPE_MAX       5
+
+/*
+ * According to PLATFORM_FIRMWARE_TYPE,
+ * Type 0x80000000 ~ 0xFFFFFFFF is reserved for OEM.
+ * A type of firmware image is determined by FirmwareType value
+ * in the Configuration of Firmware Update.
+ * Formula: FirmwareType = ImageType + 0x80000000
+ */
+#define FIRMWARE_TYPE_OEM_OFFSET 0x80000000
+
+/*
+ * Size of Firmware Descriptor File Volume
+ */
+#define FIRMWARE_DESCRIPTOR_SIZE 0x10000
+
+STATIC
+EFI_STATUS
+SmcFlashUpdate (
+  IN UINT32                                         ImageType,
+  IN VOID                                           *FirmwareImage,
+  IN UINT64                                         ImageSize,
+  IN EFI_FIRMWARE_MANAGEMENT_UPDATE_IMAGE_PROGRESS  Progress,
+  IN UINTN                                          StartPercentage,
+  IN UINTN                                          EndPercentage
+  )
+{
+  EFI_STATUS Status;
+  UINTN ProgressUpdate = StartPercentage;
+  ARM_SMC_ARGS SMCArgs = { 0 };
+
+  DEBUG ((DEBUG_INFO, "%a: Image: %p Size 0x%lx\n",
+          __FUNCTION__,
+          FirmwareImage,
+          ImageSize));
+
+  while (TRUE) {
+    SMCArgs.Arg0 = SMC_FWU_SERVICE;
+    SMCArgs.Arg1 = ImageType;
+    SMCArgs.Arg2 = (UINT64) FirmwareImage;
+    SMCArgs.Arg3 = ImageSize;
+    SMCArgs.Arg4 = 1; // SMC yield for progress reporting.
+
+    ArmCallSmc(&SMCArgs);
+    if (SMCArgs.Arg0 == SMC_FWU_IN_PROGRESS) {
+      if (NULL != Progress) {
+        Progress(ProgressUpdate);
+      }
+
+      ProgressUpdate = StartPercentage +
+          (((EndPercentage - StartPercentage) * (SMCArgs.Arg1))/100);
+      continue;
+    }
+    break;
+  }
+
+  switch (SMCArgs.Arg0) {
+    case SMC_OK:
+      if (NULL != Progress) {
+        Progress(EndPercentage);
+      }
+      Status = EFI_SUCCESS;
+      break;
+
+    case SMC_ACCESS_DENIED:
+    case SMC_AUTH_ERROR:
+      DEBUG ((DEBUG_ERROR, "%a %d Failed update. Security Violation.\n",
+              __FUNCTION__, __LINE__));
+      Status = EFI_SECURITY_VIOLATION;
+      break;
+
+    case SMC_INSUFFICIENT_RES:
+      DEBUG ((DEBUG_ERROR, "%a %d Failed update. Insufficient resources.\n",
+              __FUNCTION__, __LINE__));
+      Status = EFI_OUT_OF_RESOURCES;
+      break;
+
+    case SMC_IO_ERROR:
+      DEBUG ((DEBUG_ERROR, "%a %d Failed update. IO Error.\n",
+              __FUNCTION__, __LINE__));
+      Status = EFI_DEVICE_ERROR;
+      break;
+
+    default:
+      DEBUG ((DEBUG_ERROR, "%a %d Failed update. Unknown Error %d.\n",
+              __FUNCTION__, __LINE__, SMCArgs.Arg0));
+      Status = EFI_INVALID_PARAMETER;
+      break;
+  }
+
+  return Status;
+}
+
+/**
+  Perform flash write operation with progress indicator.  The start and end
+  completion percentage values are passed into this function.  If the requested
+  flash write operation is broken up, then completion percentage between the
+  start and end values may be passed to the provided Progress function.  The
+  caller of this function is required to call the Progress function for the
+  start and end completion percentage values.  This allows the Progress,
+  StartPercentage, and EndPercentage parameters to be ignored if the requested
+  flash write operation can not be broken up
+
+  @param[in] FirmwareType      The type of firmware.
+  @param[in] FlashAddress      The address of flash device to be accessed.
+  @param[in] FlashAddressType  The type of flash device address.
+  @param[in] Buffer            The pointer to the data buffer.
+  @param[in] Length            The length of data buffer in bytes.
+  @param[in] Progress          A function used report the progress of the
+                               firmware update.  This is an optional parameter
+                               that may be NULL.
+  @param[in] StartPercentage   The start completion percentage value that may
+                               be used to report progress during the flash
+                               write operation.
+  @param[in] EndPercentage     The end completion percentage value that may
+                               be used to report progress during the flash
+                               write operation.
+
+  @retval EFI_SUCCESS           The operation returns successfully.
+  @retval EFI_WRITE_PROTECTED   The flash device is read only.
+  @retval EFI_UNSUPPORTED       The flash device access is unsupported.
+  @retval EFI_INVALID_PARAMETER The input parameter is not valid.
+**/
+EFI_STATUS
+EFIAPI
+PerformFlashWriteWithProgress (
+  IN PLATFORM_FIRMWARE_TYPE                         FirmwareType,
+  IN EFI_PHYSICAL_ADDRESS                           FlashAddress,
+  IN FLASH_ADDRESS_TYPE                             FlashAddressType,
+  IN VOID                                           *Buffer,
+  IN UINTN                                          Length,
+  IN EFI_FIRMWARE_MANAGEMENT_UPDATE_IMAGE_PROGRESS  Progress,        OPTIONAL
+  IN UINTN                                          StartPercentage,
+  IN UINTN                                          EndPercentage
+  )
+{
+  UINT32 ImageType;
+
+  if (Buffer == NULL) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  /*
+   * As per the design of firmware capsule, the Firmware Descriptor File
+   * Volume which is used to provide description of firmware update is
+   * attached at the beginning of the firmware file. It must be removed
+   * before writing.
+   */
+  if (Length <= FIRMWARE_DESCRIPTOR_SIZE) {
+    DEBUG ((DEBUG_ERROR, "%a %d The length of firmware image is invalid.\n",
+            __FUNCTION__, __LINE__));
+    return EFI_INVALID_PARAMETER;
+  }
+  Length -= FIRMWARE_DESCRIPTOR_SIZE;
+  Buffer += FIRMWARE_DESCRIPTOR_SIZE;
+
+  ImageType = FirmwareType - FIRMWARE_TYPE_OEM_OFFSET;
+  if (ImageType >= FWU_IMG_TYPE_MAX) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  return SmcFlashUpdate (
+           ImageType,
+           Buffer,
+           Length,
+           Progress,
+           StartPercentage,
+           EndPercentage
+           );
+}
+
+/**
+  Perform flash write operation.
+
+  @param[in] FirmwareType      The type of firmware.
+  @param[in] FlashAddress      The address of flash device to be accessed.
+  @param[in] FlashAddressType  The type of flash device address.
+  @param[in] Buffer            The pointer to the data buffer.
+  @param[in] Length            The length of data buffer in bytes.
+
+  @retval EFI_SUCCESS           The operation returns successfully.
+  @retval EFI_WRITE_PROTECTED   The flash device is read only.
+  @retval EFI_UNSUPPORTED       The flash device access is unsupported.
+  @retval EFI_INVALID_PARAMETER The input parameter is not valid.
+**/
+EFI_STATUS
+EFIAPI
+PerformFlashWrite (
+  IN PLATFORM_FIRMWARE_TYPE       FirmwareType,
+  IN EFI_PHYSICAL_ADDRESS         FlashAddress,
+  IN FLASH_ADDRESS_TYPE           FlashAddressType,
+  IN VOID                         *Buffer,
+  IN UINTN                        Length
+  )
+{
+  return PerformFlashWriteWithProgress (
+           FirmwareType,
+           FlashAddress,
+           FlashAddressType,
+           Buffer,
+           Length,
+           NULL,
+           0,
+           0
+           );
+}
