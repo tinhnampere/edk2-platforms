acef0877e4e9f21fe19413ac61ceeeee32e4e42d
Vu Nguyen <Vu@amperecomputing.com>
2020-10-17 12:29:04 +0700

    AmpereAltraPkg: Add PMProLib library
    The PMProLib Library supports functions to communicate wih PMPro via
    Doorbell interface.
    
    Signed-off-by: Vu Nguyen <Vu@amperecomputing.com>
    Signed-off-by: Phong Vo <pvo@amperecomputing.com>

---
+++ Silicon/Ampere/AmpereAltraPkg/Include/Library/PMProLib.h
@@ -0,0 +1,72 @@
+/** @file
+
+  Copyright (c) 2020, Ampere Computing LLC. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _PMPROLIB_H_
+#define _PMPROLIB_H_
+
+#include <Uefi.h>
+
+/*
+ * Write the message to the doorbell
+ * Db           Doorbell number
+ * Data         Data to be written to DB OUT
+ * Param        Data to be written to DB OUT0
+ * Param1       Data to be written to DB OUT1
+ * MsgReg       Non-secure doorbell base virtual address
+ */
+EFI_STATUS
+EFIAPI
+PMProDBWr (
+  UINT8 Db,
+  UINT32 Data,
+  UINT32 Param,
+  UINT32 Param1,
+  UINT64 MsgReg
+  );
+
+/*
+ * Send Turbo enable message to PMpro
+ *
+ * Enable       Enable value
+ */
+EFI_STATUS
+EFIAPI
+PMProTurboEnable (
+  UINT8 Socket,
+  UINT8 Enable
+  );
+
+/*
+ * Send PSCI Statistic address to ACL
+ * Socket 	Socket to send address to
+ * AdrHi 	Upper 32 bits of the address
+ * AdrLo	Lower 32 bits of the address
+ */
+EFI_STATUS
+EFIAPI
+PMProAclAdd (
+  UINT8 Socket,
+  UINT32 AdrHi,
+  UINT32 AdrLo
+  );
+
+/*
+ * Send PSCI Statistic address to PMPro
+ * Socket 	Socket to send address to
+ * AdrHi 	Upper 32 bits of the address
+ * AdrLo	Lower 32 bits of the address
+ */
+EFI_STATUS
+EFIAPI
+PMProSendPSCIAddr (
+  UINT8 Socket,
+  UINT32 AdrHi,
+  UINT32 AdrLo
+  );
+
+#endif /* _PMPROLIB_H_*/
