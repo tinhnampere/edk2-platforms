e6486a5eb1b3e8a8c8cfc41ecc8290a0321b5f29
Vu Nguyen <Vu@amperecomputing.com>
2021-04-19 14:03:45 +0700

    AmpereSiliconPkg: Add functions to get/set IPMI Boot Param
    Signed-off-by: Vu Nguyen <Vu@amperecomputing.com>

--- Silicon/Ampere/AmpereSiliconPkg/Include/Library/IpmiCommandLibExt.h
+++ Silicon/Ampere/AmpereSiliconPkg/Include/Library/IpmiCommandLibExt.h
@@ -9,6 +9,8 @@
 #ifndef IPMI_COMMAND_LIB_EXT_
 #define IPMI_COMMAND_LIB_EXT_
 
+#include <IndustryStandard/Ipmi.h>
+
 #pragma pack (1)
 
 //
@@ -16,19 +18,130 @@
 //
 #define BMC_MAX_CHANNEL 0xF // 4 bits
 
+//
+// IPMI Boot Option CompletionCode
+//
+#define IPMI_COMP_CODE_PARAM_UNSUPPORTED  0x80
+#define IPMI_COMP_CODE_SET_IN_PROGRESS    0x81
+#define IPMI_COMP_CODE_READ_ONLY          0x82
+
+//
+// IPMI Boot Option param 5 (Boot Flags) info
+//
+typedef struct {
+  BOOLEAN IsBootFlagsValid;
+  BOOLEAN IsPersistent;
+  UINT8   DeviceSelector;
+  UINT8   InstanceSelector;
+} IPMI_BOOT_FLAGS_INFO;
+
 /**
-  Get BMC Lan IP Address
+  Get BMC Lan IP Address.
 
   @param[in, out]    IpAddressBuffer   A pointer to array of pointers to callee allocated buffer that returns information about BMC Lan Ip
   @param[in, out]    IpAddressSize     Size of input IpAddressBuffer. Size of Lan channel in return
 
   @retval EFI_SUCCESS                  The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval EFI_INVALID_PARAMETER        IpAddressBuffer or IpAddressSize was NULL or *IpAddressSize > Max BMC Channel
   @retval other                        Failed to write data to the device.
 **/
 EFI_STATUS
+EFIAPI
 IpmiGetBmcIpAddress (
   IN OUT IPMI_LAN_IP_ADDRESS **IpAddressBuffer,
   IN OUT UINT8               *IpAddressSize
+  );
+
+/**
+  Set BMC Boot Options parameter.
+
+  @param[in]    SetBootOptionsRequest      A pointer to an callee allocated buffer containing request data.
+  @param[in]    SetBootOptionsRequestSize  Size of SetBootOptionsRequest.
+  @param[out]   CompletionCode             Returned Completion Code from BMC.
+
+  @retval EFI_SUCCESS                      The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval EFI_INVALID_PARAMETER            SetBootOptionsRequest or CompletionCode was NULL.
+  @retval other                            Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiSetSystemBootOptions (
+  IN  IPMI_SET_BOOT_OPTIONS_REQUEST  *SetBootOptionsRequest,
+  IN  UINT32                         SetBootOptionsRequestSize,
+  OUT UINT8                          *CompletionCode
+  );
+
+/**
+  Get BMC Boot Options parameter.
+
+  @param[in]        ParameterSelector           Boot Options parameter which want to get.
+  @param[out]       GetBootOptionsResponse      A pointer to an callee allocated buffer to store response data.
+  @param[in, out]   GetBootOptionsResponseSize  Size of GetBootOptionsResponse.
+
+  @retval EFI_SUCCESS                           The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval EFI_INVALID_PARAMETER                 GetBootOptionsResponse or GetBootOptionsResponseSize was NULL.
+  @retval other                                 Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiGetSystemBootOptions (
+  IN     UINT8                          ParameterSelector,
+  OUT    IPMI_GET_BOOT_OPTIONS_RESPONSE *GetBootOptionsResponse,
+  IN OUT UINT32                         *GetBootOptionsResponseSize
+  );
+
+/**
+  Set Boot Info Acknowledge to notify BMC that the Boot Flags has been handled by UEFI.
+
+  @retval EFI_SUCCESS                           The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval other                                 Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiSetBootInfoAck (
+  VOID
+  );
+
+/**
+  Retrieve Boot Info Acknowledge from BMC.
+
+  @param[out] BootInitiatorAcknowledgeData      Pointer to returned buffer.
+
+  @retval EFI_SUCCESS                           The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval EFI_INVALID_PARAMETER                 BootInitiatorAcknowledgeData was NULL.
+  @retval other                                 Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiGetBootInfoAck (
+  OUT UINT8 *BootInitiatorAcknowledgeData
+  );
+
+/**
+  Send command to clear BMC Boot Flags parameter.
+
+  @retval EFI_SUCCESS                           The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval other                                 Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiClearBootFlags (
+  VOID
+  );
+
+/**
+  Retrieve Boot Flags parameter from BMC.
+
+  @param[out] BootFlags                         Pointer to returned buffer.
+
+  @retval EFI_SUCCESS                           The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval EFI_INVALID_PARAMETER                 BootFlags was NULL.
+  @retval other                                 Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiGetBootFlags (
+  OUT IPMI_BOOT_FLAGS_INFO *BootFlags
   );
 
 #pragma pack()
--- Silicon/Ampere/AmpereSiliconPkg/Library/IpmiCommandLibExt/IpmiCommandLibExt.c
+++ Silicon/Ampere/AmpereSiliconPkg/Library/IpmiCommandLibExt/IpmiCommandLibExt.c
@@ -9,8 +9,6 @@
 #include <Uefi.h>
 
 #include <IndustryStandard/Ipmi.h>
-#include <IndustryStandard/IpmiNetFnApp.h>
-#include <IndustryStandard/IpmiNetFnTransport.h>
 #include <IpmiNetFnAppExt.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
@@ -19,15 +17,17 @@
 #include <Library/MemoryAllocationLib.h>
 
 /**
-  Get BMC Lan IP Address
+  Get BMC Lan IP Address.
 
   @param[in, out]    IpAddressBuffer   A pointer to array of pointers to callee allocated buffer that returns information about BMC Lan Ip
   @param[in, out]    IpAddressSize     Size of input IpAddressBuffer. Size of Lan channel in return
 
   @retval EFI_SUCCESS                  The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval EFI_INVALID_PARAMETER        IpAddressBuffer or IpAddressSize was NULL or *IpAddressSize > Max BMC Channel
   @retval other                        Failed to write data to the device.
 **/
 EFI_STATUS
+EFIAPI
 IpmiGetBmcIpAddress (
   IN OUT IPMI_LAN_IP_ADDRESS **IpAddressBuffer,
   IN OUT UINT8               *IpAddressSize
@@ -43,8 +43,12 @@ IpmiGetBmcIpAddress (
   UINT8                                          ChannelNumber;
   UINT8                                          Count;
 
-  ASSERT (IpAddressBuffer != NULL && IpAddressSize != NULL);
-  ASSERT (*IpAddressSize <= BMC_MAX_CHANNEL);
+  if (IpAddressBuffer == NULL
+      || IpAddressSize == NULL
+      || *IpAddressSize > BMC_MAX_CHANNEL)
+  {
+    return EFI_INVALID_PARAMETER;
+  }
 
   Count = 0;
   GetConfigurationParametersResponse = AllocateZeroPool (
@@ -154,5 +158,296 @@ IpmiGetBmcIpAddress (
 
 Exit:
   FreePool (GetConfigurationParametersResponse);
+  return Status;
+}
+
+/**
+  Set BMC Boot Options parameter.
+
+  @param[in]    SetBootOptionsRequest      A pointer to an callee allocated buffer containing request data.
+  @param[in]    SetBootOptionsRequestSize  Size of SetBootOptionsRequest.
+  @param[out]   CompletionCode             Returned Completion Code from BMC.
+
+  @retval EFI_SUCCESS                      The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval EFI_INVALID_PARAMETER            SetBootOptionsRequest or CompletionCode was NULL.
+  @retval other                            Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiSetSystemBootOptions (
+  IN  IPMI_SET_BOOT_OPTIONS_REQUEST  *SetBootOptionsRequest,
+  IN  UINT32                         SetBootOptionsRequestSize,
+  OUT UINT8                          *CompletionCode
+  )
+{
+  EFI_STATUS                    Status;
+  UINT32                        DataSize;
+
+  if (SetBootOptionsRequest == NULL
+      || CompletionCode == NULL)
+  {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  DataSize = sizeof (*CompletionCode);
+  Status = IpmiSubmitCommand (
+             IPMI_NETFN_CHASSIS,
+             IPMI_CHASSIS_SET_SYSTEM_BOOT_OPTIONS,
+             (VOID *)SetBootOptionsRequest,
+             SetBootOptionsRequestSize,
+             (VOID *)CompletionCode,
+             &DataSize
+             );
+
+  switch (*CompletionCode) {
+  case IPMI_COMP_CODE_NORMAL:
+    break;
+
+  case IPMI_COMP_CODE_PARAM_UNSUPPORTED:
+    Status = EFI_UNSUPPORTED;
+    break;
+
+  case IPMI_COMP_CODE_SET_IN_PROGRESS:
+    Status = EFI_NOT_READY;
+    break;
+
+  case IPMI_COMP_CODE_READ_ONLY:
+    Status = EFI_ACCESS_DENIED;
+    break;
+
+  default:
+    Status = EFI_DEVICE_ERROR;
+    break;
+  }
+
+  return Status;
+}
+
+/**
+  Get BMC Boot Options parameter.
+
+  @param[in]        ParameterSelector           Boot Options parameter which want to get.
+  @param[out]       GetBootOptionsResponse      A pointer to an callee allocated buffer to store response data.
+  @param[in, out]   GetBootOptionsResponseSize  Size of GetBootOptionsResponse.
+
+  @retval EFI_SUCCESS                           The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval EFI_INVALID_PARAMETER                 GetBootOptionsResponse or GetBootOptionsResponseSize was NULL.
+  @retval other                                 Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiGetSystemBootOptions (
+  IN     UINT8                          ParameterSelector,
+  OUT    IPMI_GET_BOOT_OPTIONS_RESPONSE *GetBootOptionsResponse,
+  IN OUT UINT32                         *GetBootOptionsResponseSize
+  )
+{
+  EFI_STATUS                    Status;
+  IPMI_GET_BOOT_OPTIONS_REQUEST GetBootOptionsRequest;
+
+  if (GetBootOptionsResponse == NULL
+      || GetBootOptionsResponseSize == NULL)
+  {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  GetBootOptionsRequest.ParameterSelector.Bits.ParameterSelector = ParameterSelector;
+  GetBootOptionsRequest.ParameterSelector.Bits.Reserved = 0;
+  GetBootOptionsRequest.SetSelector = 0;
+  GetBootOptionsRequest.BlockSelector = 0;
+
+  Status = IpmiSubmitCommand (
+             IPMI_NETFN_CHASSIS,
+             IPMI_CHASSIS_GET_SYSTEM_BOOT_OPTIONS,
+             (VOID *)&GetBootOptionsRequest,
+             sizeof (GetBootOptionsRequest),
+             (VOID *)GetBootOptionsResponse,
+             GetBootOptionsResponseSize
+             );
+
+  switch (GetBootOptionsResponse->CompletionCode) {
+  case IPMI_COMP_CODE_NORMAL:
+    break;
+
+  case IPMI_COMP_CODE_PARAM_UNSUPPORTED:
+    Status = EFI_UNSUPPORTED;
+    break;
+
+  default:
+    Status = EFI_DEVICE_ERROR;
+    break;
+  }
+
+  return Status;
+}
+
+/**
+  Set Boot Info Acknowledge to notify BMC that the Boot Flags has been handled by UEFI.
+
+  @retval EFI_SUCCESS                           The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval other                                 Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiSetBootInfoAck (
+  VOID
+  )
+{
+  EFI_STATUS                             Status;
+  IPMI_BOOT_OPTIONS_RESPONSE_PARAMETER_4 *ParameterData;
+  IPMI_SET_BOOT_OPTIONS_REQUEST          *SetBootOptionsRequest;
+  UINT32                                 SetBootOptionsRequestSize;
+  UINT8                                  CompletionCode;
+
+  SetBootOptionsRequestSize = sizeof (*SetBootOptionsRequest) + sizeof (*ParameterData);
+  SetBootOptionsRequest = AllocateZeroPool (SetBootOptionsRequestSize);
+  if (SetBootOptionsRequest == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  SetBootOptionsRequest->ParameterValid.Bits.ParameterSelector = IPMI_BOOT_OPTIONS_PARAMETER_BOOT_INFO_ACK;
+  SetBootOptionsRequest->ParameterValid.Bits.MarkParameterInvalid = 0x0;
+
+  ParameterData = (IPMI_BOOT_OPTIONS_RESPONSE_PARAMETER_4 *)&SetBootOptionsRequest->ParameterData;
+  ParameterData->WriteMask = BIT0;
+  ParameterData->BootInitiatorAcknowledgeData = 0x0;
+
+  Status = IpmiSetSystemBootOptions (
+             SetBootOptionsRequest,
+             SetBootOptionsRequestSize,
+             &CompletionCode
+             );
+
+  FreePool (SetBootOptionsRequest);
+  return Status;
+}
+
+/**
+  Retrieve Boot Info Acknowledge from BMC.
+
+  @param[out] BootInitiatorAcknowledgeData      Pointer to returned buffer.
+
+  @retval EFI_SUCCESS                           The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval EFI_INVALID_PARAMETER                 BootInitiatorAcknowledgeData was NULL.
+  @retval other                                 Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiGetBootInfoAck (
+  OUT UINT8 *BootInitiatorAcknowledgeData
+  )
+{
+  EFI_STATUS                              Status;
+  IPMI_BOOT_OPTIONS_RESPONSE_PARAMETER_4  *ParameterData;
+  IPMI_GET_BOOT_OPTIONS_RESPONSE          *GetBootOptionsResponse;
+  UINT32                                  GetBootOptionsResponseSize;
+
+  if (BootInitiatorAcknowledgeData == NULL) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  GetBootOptionsResponseSize = sizeof (*GetBootOptionsResponse) + sizeof (*ParameterData);
+  GetBootOptionsResponse = AllocateZeroPool (GetBootOptionsResponseSize);
+  if (GetBootOptionsResponse == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  ParameterData = (IPMI_BOOT_OPTIONS_RESPONSE_PARAMETER_4 *)&GetBootOptionsResponse->ParameterData;
+
+  Status = IpmiGetSystemBootOptions (
+             IPMI_BOOT_OPTIONS_PARAMETER_BOOT_INFO_ACK,
+             GetBootOptionsResponse,
+             &GetBootOptionsResponseSize
+             );
+
+  if (!EFI_ERROR (Status)) {
+    *BootInitiatorAcknowledgeData = ParameterData->BootInitiatorAcknowledgeData;
+  }
+
+  FreePool (GetBootOptionsResponse);
+  return Status;
+}
+
+/**
+  Send command to clear BMC Boot Flags parameter.
+
+  @retval EFI_SUCCESS                           The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval other                                 Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiClearBootFlags (
+  VOID
+  )
+{
+  EFI_STATUS                              Status;
+  IPMI_SET_BOOT_OPTIONS_REQUEST           *SetBootOptionsRequest;
+  UINT32                                  SetBootOptionsRequestSize;
+  UINT8                                   CompletionCode;
+
+  SetBootOptionsRequestSize = sizeof (*SetBootOptionsRequest) + sizeof (IPMI_BOOT_OPTIONS_RESPONSE_PARAMETER_5);
+  SetBootOptionsRequest = AllocateZeroPool (SetBootOptionsRequestSize);
+  if (SetBootOptionsRequest == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  SetBootOptionsRequest->ParameterValid.Bits.ParameterSelector = IPMI_BOOT_OPTIONS_PARAMETER_BOOT_FLAGS;
+
+  Status = IpmiSetSystemBootOptions (
+             SetBootOptionsRequest,
+             SetBootOptionsRequestSize,
+             &CompletionCode
+             );
+
+  FreePool (SetBootOptionsRequest);
+  return Status;
+}
+
+/**
+  Retrieve Boot Flags parameter from BMC.
+
+  @param[out] BootFlags                         Pointer to returned buffer.
+
+  @retval EFI_SUCCESS                           The command byte stream was successfully submit to the device and a response was successfully received.
+  @retval EFI_INVALID_PARAMETER                 BootFlags was NULL.
+  @retval other                                 Failed to write data to the device.
+**/
+EFI_STATUS
+EFIAPI
+IpmiGetBootFlags (
+  OUT IPMI_BOOT_FLAGS_INFO *BootFlags
+  )
+{
+  EFI_STATUS                              Status;
+  IPMI_BOOT_OPTIONS_RESPONSE_PARAMETER_5  *ParameterData;
+  IPMI_GET_BOOT_OPTIONS_RESPONSE          *GetBootOptionsResponse;
+  UINT32                                  GetBootOptionsResponseSize;
+
+  if (BootFlags == NULL) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  GetBootOptionsResponseSize = sizeof (*GetBootOptionsResponse) + sizeof (*ParameterData);
+  GetBootOptionsResponse = AllocateZeroPool (GetBootOptionsResponseSize);
+  if (GetBootOptionsResponse == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  ParameterData = (IPMI_BOOT_OPTIONS_RESPONSE_PARAMETER_5 *)&GetBootOptionsResponse->ParameterData;
+
+  Status = IpmiGetSystemBootOptions (
+             IPMI_BOOT_OPTIONS_PARAMETER_BOOT_FLAGS,
+             GetBootOptionsResponse,
+             &GetBootOptionsResponseSize
+             );
+
+  if (!EFI_ERROR (Status)) {
+    BootFlags->IsPersistent = ParameterData->Data1.Bits.PersistentOptions;
+    BootFlags->IsBootFlagsValid = ParameterData->Data1.Bits.BootFlagValid;
+    BootFlags->DeviceSelector = ParameterData->Data2.Bits.BootDeviceSelector;
+    BootFlags->InstanceSelector = ParameterData->Data5.Bits.DeviceInstanceSelector;
+  }
+
+  FreePool (GetBootOptionsResponse);
   return Status;
 }
