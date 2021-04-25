42dc92b26937d7226d8597e21287cd3ba282ba80
Nhi Pham <npham@amperecomputing.com>
2021-04-25 11:42:12 +0700

    JadePkg: ACPI SLIT: Correct NUMA node distances
    Currently all NUMA nodes within a socket are equidistant from a given
    node. This does not reflect the latencies in the hardware due to mesh
    routing.
    
    This patch computes distance from a node to another NUMA node in the
    SLIT to better differentiate between various NUMA nodes.
    
    Below are an example output from numactl on CentOS 8.3.2011 runing
    Linux kernel 4.18.0-240.el8.aarch64.
    
    [root@localhost ~]# numactl -H
    available: 8 nodes (0-7)
    node 0 cpus: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
    node 0 size: 31947 MB
    node 0 free: 31677 MB
    node 1 cpus: 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39
    node 1 size: 16355 MB
    node 1 free: 15968 MB
    node 2 cpus: 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59
    node 2 size: 32723 MB
    node 2 free: 31750 MB
    node 3 cpus: 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79
    node 3 size: 16355 MB
    node 3 free: 15732 MB
    node 4 cpus: 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95
    node 4 size: 31702 MB
    node 4 free: 31313 MB
    node 5 cpus: 96 97 98 99 100 101 102 103 104 105 106 107 108 109 110 111
    node 5 size: 16357 MB
    node 5 free: 16085 MB
    node 6 cpus: 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127
    node 6 size: 32725 MB
    node 6 free: 32520 MB
    node 7 cpus: 128 129 130 131 132 133 134 135 136 137 138 139 140 141 142 143
    node 7 size: 16251 MB
    node 7 free: 16002 MB
    node distances:
    node   0   1   2   3   4   5   6   7
      0:  10  11  11  12  20  20  21  21
      1:  11  10  12  11  20  20  21  21
      2:  11  12  10  11  21  21  22  22
      3:  12  11  11  10  21  21  22  22
      4:  20  20  21  21  10  11  11  12
      5:  20  20  21  21  11  10  12  11
      6:  21  21  22  22  11  12  10  11
      7:  21  21  22  22  12  11  11  10
    
    Signed-off-by: Nhi Pham <npham@amperecomputing.com>

--- Platform/Ampere/JadePkg/Drivers/AcpiPlatformDxe/AcpiSLIT.c
+++ Platform/Ampere/JadePkg/Drivers/AcpiPlatformDxe/AcpiSLIT.c
@@ -8,6 +8,7 @@
 
 #include "AcpiPlatform.h"
 
+#define MAX_NODES_PER_SOCKET          4
 #define SELF_DISTANCE                 10
 #define REMOTE_DISTANCE               20
 
@@ -19,6 +20,117 @@ EFI_ACPI_6_3_SYSTEM_LOCALITY_DISTANCE_INFORMATION_TABLE_HEADER SLITTableHeaderTe
     ),
   0,
 };
+
+VOID
+ComputeCoordinatesForNode (
+  UINTN Node,
+  UINTN *X,
+  UINTN *Y
+  )
+{
+  switch (Node) {
+  case 0:
+    *X = 0;
+    *Y = 0;
+    break;
+  case 1:
+    *X = 1;
+    *Y = 0;
+    break;
+  case 2:
+    *X = 0;
+    *Y = 1;
+    break;
+  case 3:
+    *X = 1;
+    *Y = 1;
+    break;
+  default:
+    *X = 0;
+    *Y = 0;
+    break;
+  }
+}
+
+/**
+  Compute the distance between between two nodes on socket.
+**/
+UINT8
+ComputeSlitDistanceOnSocket (
+  UINTN Node1,
+  UINTN Node2
+  )
+{
+  UINTN X1, Y1, X2, Y2;
+  UINTN XDistance, YDistance;
+
+  ComputeCoordinatesForNode (Node1, &X1, &Y1);
+  ComputeCoordinatesForNode (Node2, &X2, &Y2);
+
+  XDistance = ABS ((INTN)(X1 - X2));
+  YDistance = ABS ((INTN)(Y1 - Y2));
+
+  return (UINT8)(XDistance + YDistance + SELF_DISTANCE);
+}
+
+/**
+  Compute the distance between between two nodes on
+  different sockets.
+  Node1 - local socket node number
+  Node2 - remote socket node number
+**/
+UINT8
+ComputeSlitDistanceOnRemoteSocket (
+  UINTN LocalNode,
+  UINTN RemoteNode
+  )
+{
+  UINTN LocalDistance, RemoteDistance;
+
+  //
+  // Mesh forwards traffic between sockets over both CCIX links when going from
+  // one quadrant to another. For example, memory access from Node 0 to Node 4
+  // results in traffic being split between RCA0 and 1. Hence distance is
+  // different only between upper half and lower half of sockets and not
+  // between quadrants. Hemisphere configuration is not impacted as there
+  // is no upper-half.
+  //
+  LocalDistance = 0;
+  RemoteDistance = 0;
+  if (LocalNode >= (MAX_NODES_PER_SOCKET / 2)) {
+    LocalDistance = 1;
+  }
+  if (RemoteNode >= (MAX_NODES_PER_SOCKET / 2)) {
+    RemoteDistance = 1;
+  }
+
+  return (UINT8)(LocalDistance + RemoteDistance + REMOTE_DISTANCE);
+}
+
+UINT8
+ComputeSlitDistance (
+  UINTN Node1,
+  UINTN Node2,
+  UINTN DomainsPerSocket
+  )
+{
+  UINT8 Distance;
+
+  Distance = 0;
+  if ((Node1 / DomainsPerSocket) == (Node2 / DomainsPerSocket)) {
+    Distance = ComputeSlitDistanceOnSocket (
+                 Node1 % DomainsPerSocket,
+                 Node2 % DomainsPerSocket
+                 );
+  } else {
+    Distance = ComputeSlitDistanceOnRemoteSocket (
+                 Node1 % DomainsPerSocket,
+                 Node2 % DomainsPerSocket
+                 );
+  }
+
+  return Distance;
+}
 
 EFI_STATUS
 AcpiInstallSlitTable (
@@ -55,15 +167,7 @@ AcpiInstallSlitTable (
   TmpPtr = (UINT8 *)SlitTablePointer + sizeof (SLITTableHeaderTemplate);
   for (Count = 0; Count < NumDomain; Count++) {
     for (Count1 = 0; Count1 < NumDomain; Count1++, TmpPtr++) {
-      if (Count == Count1) {
-        *TmpPtr = (UINT8)SELF_DISTANCE;
-        continue;
-      }
-      if ((Count1 / NumDomainPerSocket) == (Count / NumDomainPerSocket)) {
-        *TmpPtr = (UINT8)(SELF_DISTANCE + 1);
-      } else {
-        *TmpPtr = (UINT8)(REMOTE_DISTANCE);
-      }
+      *TmpPtr = ComputeSlitDistance (Count, Count1, NumDomainPerSocket);
     }
   }
 
