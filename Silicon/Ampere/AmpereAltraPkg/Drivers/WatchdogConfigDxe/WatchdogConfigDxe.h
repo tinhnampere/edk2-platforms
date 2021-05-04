ef085bd4664b52f3ca0714b393cdc3ca3a8207f3
Vu Nguyen <vunguyen@os.amperecomputing.com>
2021-05-04 20:10:26 +0700

    AmpereAltraPkg: Add configuration screen for Watchdog timer
    This adds an option to configure the timeout of the secure watchdog timer
    which are used to monitor the pre-UEFI system booting. The system will
    be reset if the timer expires. The default timeout is 5 minutes.
    
    Cc: Thang Nguyen <thang@os.amperecomputing.com>
    Cc: Chuong Tran <chuong@os.amperecomputing.com>
    Cc: Phong Vo <phong@os.amperecomputing.com>
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Nate DeSimone <nathaniel.l.desimone@intel.com>
    
    Signed-off-by: Nhi Pham <nhi@os.amperecomputing.com>

---
+++ Silicon/Ampere/AmpereAltraPkg/Drivers/WatchdogConfigDxe/WatchdogConfigDxe.h
@@ -0,0 +1,73 @@
+/** @file
+
+  Copyright (c) 2020 - 2021, Ampere Computing LLC. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef WATCHDOG_CONFIG_DXE_H_
+#define WATCHDOG_CONFIG_DXE_H_
+
+#include <Uefi.h>
+
+#include <Protocol/HiiConfigAccess.h>
+#include <Protocol/HiiConfigRouting.h>
+
+#include "WatchdogConfigNVDataStruct.h"
+
+//
+// This is the generated IFR binary data for each formset defined in VFR.
+//
+extern UINT8 WatchdogConfigVfrBin[];
+
+//
+// This is the generated String package data for all .UNI files.
+//
+extern UINT8 WatchdogConfigDxeStrings[];
+
+#define WATCHDOG_CONFIG_PRIVATE_SIGNATURE SIGNATURE_32 ('W', 'D', 'T', 'C')
+
+typedef struct {
+  UINTN Signature;
+
+  EFI_HANDLE                    DriverHandle;
+  EFI_HII_HANDLE                HiiHandle;
+  WATCHDOG_CONFIG_VARSTORE_DATA Configuration;
+
+  //
+  // Consumed protocol
+  //
+  EFI_HII_CONFIG_ROUTING_PROTOCOL *HiiConfigRouting;
+
+  //
+  // Produced protocol
+  //
+  EFI_HII_CONFIG_ACCESS_PROTOCOL ConfigAccess;
+} WATCHDOG_CONFIG_PRIVATE_DATA;
+
+#define WATCHDOG_CONFIG_PRIVATE_FROM_THIS(a)  CR (a, WATCHDOG_CONFIG_PRIVATE_DATA, ConfigAccess, WATCHDOG_CONFIG_PRIVATE_SIGNATURE)
+
+#pragma pack(1)
+
+///
+/// HII specific Vendor Device Path definition.
+///
+typedef struct {
+  VENDOR_DEVICE_PATH       VendorDevicePath;
+  EFI_DEVICE_PATH_PROTOCOL End;
+} HII_VENDOR_DEVICE_PATH;
+
+#pragma pack()
+
+EFI_STATUS
+WatchdogConfigNvParamSet (
+  IN WATCHDOG_CONFIG_VARSTORE_DATA *VarStoreConfig
+  );
+
+EFI_STATUS
+WatchdogConfigNvParamGet (
+  OUT WATCHDOG_CONFIG_VARSTORE_DATA *VarStoreConfig
+  );
+
+#endif /* WATCHDOG_CONFIG_DXE_H_ */
---
+++ Silicon/Ampere/AmpereAltraPkg/Drivers/WatchdogConfigDxe/WatchdogConfigNVDataStruct.h
@@ -0,0 +1,25 @@
+/** @file
+
+  Copyright (c) 2020 - 2021, Ampere Computing LLC. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef WATCHDOG_CONFIG_NV_DATA_STRUCT_H_
+#define WATCHDOG_CONFIG_NV_DATA_STRUCT_H_
+
+#include <Guid/WatchdogConfigHii.h>
+
+#define WATCHDOG_CONFIG_VARSTORE_ID       0x1234
+#define WATCHDOG_CONFIG_FORM_ID           0x1235
+
+#define SWDT_DEFAULT_VALUE                300 // 5 minutes
+
+#pragma pack(1)
+typedef struct {
+  UINT32 SecureWatchdogTimerTimeout;
+} WATCHDOG_CONFIG_VARSTORE_DATA;
+#pragma pack()
+
+#endif /* WATCHDOG_CONFIG_NV_DATA_STRUCT_H_ */
